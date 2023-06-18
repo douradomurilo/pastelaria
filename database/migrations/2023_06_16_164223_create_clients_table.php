ace66f4d0ef2b8dfa34a68aee1ede7630fce1d3a
Murilo Dourado <murilo.dcarmo@gmail.com>
2023-06-18 19:54:09 -0300

    First migrations created

---
+++ database/migrations/2023_06_16_164223_create_clients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->date('birthdate');
+            $table->text('address');
+            $table->string('address_complement');
+            $table->string('district');
+            $table->string('zipcode');
+            
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
