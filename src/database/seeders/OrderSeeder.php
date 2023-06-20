123624e649baa78ceb83d857458a2299a2a2ea55
Murilo Dourado <murilo.dcarmo@gmail.com>
2023-06-20 15:28:46 -0300

    Seeders created

---
+++ src/database/seeders/OrderSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Order;
+use Illuminate\Database\Seeder;
+
+class OrderSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Order::factory()
+            ->count(50)
+            ->create();
+    }
+}
